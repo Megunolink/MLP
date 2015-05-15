6a823fe48f57057a411876893216d3e5b9f3c013
Paul <pmartinsen@gmail.com>
2015-05-15 20:10:00 +1200

    Changed slashes in MegunoLink header

--- MegunoLink.h
+++ MegunoLink.h
@@ -1,7 +1,7 @@
 #define __PROG_TYPES_COMPAT__
-#include "utility\InterfacePanel.h"
-#include "utility\Map.h"
-#include "utility\Message.h"
-#include "utility\Table.h"
-#include "utility\TimePlot.h"
-#include "utility\XYPlot.h"
+#include "utility/InterfacePanel.h"
+#include "utility/Map.h"
+#include "utility/Message.h"
+#include "utility/Table.h"
+#include "utility/TimePlot.h"
+#include "utility/XYPlot.h" 
