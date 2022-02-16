505fcfec2a5f53dc879d3299fce69d7be5bf3bc9
Paul <Paul.Martinsen@NumberEightInnovation.com>
2022-02-16 19:59:47 +1300

    Tidied up UDP plotting example.

--- examples/TimePlot/ESP8266UDPWirelessPlotting/ESP8266UDPWirelessPlotting.ino
+++ examples/TimePlot/ESP8266UDPWirelessPlotting/ESP8266UDPWirelessPlotting.ino
@@ -1,64 +1,97 @@
 /************************************************************************************************
-Example Description
-Data is sent from an ESP8266 wifi module over UDP to MegunoLink where it is plotted in real time
+In this example, data is sent from an ESP8266 wifi module over UDP to MegunoLink where it is 
+plotted in real time. The example uses the UDP broadcast address so it doesn't need to know
+the IP address of the computer running MegunoLink. Open the ESP8266UDPWirelessPlotting interface
+in MegunoLink to view the data. You'll find it inside the same folder as this Arduino code.
 
 More Information
-*  http://www.megunolink.com/documentation/plotting/
+  * Getting started with MegunoLink: https://www.megunolink.com/documentation/getting-started/
+  * Creating UDP connections: https://www.megunolink.com/documentation/connecting/udp-connections/
+  * Plotting reference: http://www.megunolink.com/documentation/plotting/
 
 This Example Requires:
-*  The MegunoLink arduino library https://www.megunolink.com/documentation/arduino-integration/
-*  The WiFi Manager library https://github.com/tzapu/WiFiManager
-*  The ESP8266 arduino library and files https://github.com/esp8266/Arduino
-
-
-MegunoLink Interface
-You can download a pre-made interface from here:
-https://github.com/Megunolink/MLP/raw/master/examples/TimePlot/ESP8266UDPWirelessPlotting/ESP8266UDPWirelessPlotting.mlpz
+  * The MegunoLink arduino library https://www.megunolink.com/documentation/arduino-integration/
+  * The ESP8266 arduino library and files https://github.com/esp8266/Arduino
 
 You can find out more about MegunoLink and download a free trial from here
-https://www.megunolink.com/
-https://www.megunolink.com/download/
+  * https://www.megunolink.com/
+  * https://www.megunolink.com/download/
 ************************************************************************************************/
 
-#include "DNSServer.h"
-#include "ESP8266WebServer.h"
-#include "WiFiManager.h"
 #include "ESP8266WiFi.h"
 #include "WiFiUdp.h"
 
 #include "MegunoLink.h"
+#include "ArduinoTimer.h"
 
+// Setup WiFi credentials (SSID and password). 
+#define USE_WIFI_CONFIG_FILE // Comment out this line to use Option 2 below
+#ifdef USE_WIFI_CONFIG_FILE
+
+// Include SSID and password from a library file. See:
+// https://www.megunolink.com/articles/wireless/how-do-i-connect-to-a-wireless-network-with-the-esp32/
+#include "WiFiConfig.h"
+
+#else
+
+// Option 2
+const char *SSID = "Your SSID";
+const char *WiFiPassword = "Your Password";
+
+#endif
+
+// Use the broadcast address for the destination computer
+// running MegunoLink so we don't have to know its
+// IP address/name.
+const IPAddress DestinationIp(255, 255, 255, 255);
 const int SourcePort = 52790;
 const int DestinationPort = 52791;
-const IPAddress DestinationIp(255, 255, 255, 255);
 
-WiFiManager ConnectionManager;
 WiFiUDP UdpConnection;
+ArduinoTimer SendTimer;
 
 void setup()
 {
   Serial.begin(9600);
-  Serial.println("WiFi Test");
+  Serial.println("UDP Plotting Test");
 
-  Serial.println("Connecting");
-  ConnectionManager.autoConnect();
-  Serial.println("Connected");
-
-  UdpConnection.begin(SourcePort);
+  // Setup the WiFi connection, waiting until it is ready. 
+  WiFi.begin(SSID, WiFiPassword);
+  auto Result = WiFi.waitForConnectResult();
+  if (Result == WL_CONNECTED)
+  {
+    Serial.print(F("Connected. My IP address: "));
+    Serial.println(WiFi.localIP());
+    UdpConnection.begin(SourcePort);
+  }
+  else
+  {
+    Serial.print(F("Couldn't connect to WiFi. Error: "));
+    switch(Result)
+    {
+      case WL_NO_SHIELD: Serial.println(F("No wifi shield")); break;
+      case WL_NO_SSID_AVAIL: Serial.print(F("SSID not found-")); Serial.println(SSID); break;
+      case WL_CONNECT_FAILED: Serial.print(F("connect failed")); break;
+      case WL_CONNECTION_LOST: Serial.print(F("connection lost")); break;
+      default: Serial.println(Result); break;
+    }
+  }
 }
 
 void loop()
 {
-  // Send a message
-  UdpConnection.beginPacket(DestinationIp, DestinationPort);
-  UdpConnection.println("A wireless hello");
-  UdpConnection.endPacket();
-
-  // Send data for plotting
-  UdpConnection.beginPacket(DestinationIp, DestinationPort);
-  TimePlot Plot("ADC", UdpConnection);
-  Plot.SendData("A0", analogRead(A0));
-  UdpConnection.endPacket();
-
-  delay(500); // ms
+  // Periodically send data when the connection is available. 
+  if (WiFi.isConnected() && SendTimer.TimePassed_Milliseconds(400))
+  {
+    // Send a message
+    UdpConnection.beginPacket(DestinationIp, DestinationPort);
+    UdpConnection.println("A wireless hello");
+    UdpConnection.endPacket();
+  
+    // Send data for plotting
+    UdpConnection.beginPacket(DestinationIp, DestinationPort);
+    TimePlot Plot("ADC", UdpConnection);
+    Plot.SendData("A0", analogRead(A0));
+    UdpConnection.endPacket();
+  }
 }
