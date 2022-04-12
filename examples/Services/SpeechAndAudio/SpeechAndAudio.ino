df9369f812ce382480060acc785976a8ad4ee880
Paul <Paul.Martinsen@NumberEightInnovation.com>
2022-04-13 11:04:40 +1200

    Added speech and audio example.

---
+++ examples/Services/SpeechAndAudio/SpeechAndAudio.ino
@@ -0,0 +1,38 @@
+/* **************************************
+ *  This example demonstrates text-to-
+ *  speech and audio playback in MegunoLink.
+ *  Open the companion MegunoLink project file
+ *  (SpeechAndAudio.mlpz) in the same folder
+ *  as this Arduino sketch. 
+ *  
+ *  For more information see: 
+ *    https://www.megunolink.com/documentation/play-sounds/
+ *    https://www.megunolink.com/documentation/arduino-libraries/arduino-timer/
+ */
+#include "MegunoLink.h"
+#include "ArduinoTimer.h"
+
+int nCounter = 10; 
+ArduinoTimer Countdown;
+Audio Sounds;
+
+void setup() 
+{
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+  if (Countdown.TimePassed_Seconds(2))
+  {
+    if (nCounter > 0)
+    { 
+      Sounds.Speak(nCounter);
+    }
+    if (nCounter == 0)
+    {
+      Sounds.PlayAudioClip(F("Explosion"));
+    }
+    --nCounter;
+  }
+}
